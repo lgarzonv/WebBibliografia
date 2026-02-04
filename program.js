f4919ffbeab01a483758fc361cce00517800510e
Liz <lgarzonv@unal.edu.co>
2026-02-03 20:39:12 -0500

    1

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([4.642365566284839, -74.15616023510594], 13);
+var map = L.map('map').setView([4.642365566284839, -74.15616023510594], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
